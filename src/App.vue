3b3abf681b2ff648a076c8b5d14cf438745a0609
Ruth <ruth@Deniss-Air.home>
2022-02-06 12:11:12 +0100

    numeric input prop and emit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <NumericInput :value="value" @onincrease="increaseValue" @ondecrease="decreaseValue"/> this is not optimal -->
+    <!-- bananas= $event : event is the second parameter in the emit , aka new value/quantity -->
+    bananas <NumericInput :value="bananas" :minimum="1" :maximum="5" @onchange="bananas = $event"/> 
+    apples <NumericInput :value="apples" :minimum="2" :maximum="5" @onchange="apples =$event"/>
+    lemons <NumericInput :value="lemons" :minimum="4"  :maximum="10" @onchange="lemons =$event"/> 
+    pineapples <NumericInput :value="pineapples" :minimum="1" :maximum="8" @onchange="pineapples =$event"/> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NumericInput from './components/NumericInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NumericInput
+  },
+  data(){
+    return {
+      bananas:1,
+      apples:2,
+      lemons:4,
+      pineapples:1
+      // value:1
+    }
   }
 }
 </script>
---
+++ src/components/NumericInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="increase">+</button>
+    {{value}}
+    <button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumericInput',
+  props:{
+    value: Number,
+    minimum : Number,
+    maximum: Number
+  },
+  methods:{
+    increase(){
+      this.$emit("onchange", this.value < this.maximum ? this.value +1: this.value)
+    }, 
+    decrease(){
+      this.$emit("onchange", this.value > this.minimum? this.value -1: this.value)
+      // mutations: +=, =, *=, ++, --, /=
+      // statements: *, +, -,  / they dont mutate
+    }
+  },
+  emits:['onchange'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
